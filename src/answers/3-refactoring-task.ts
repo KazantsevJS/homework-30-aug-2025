ef0ad3f4fc70acbce0b66f6410e8119ddfe29632
pauzhetsky <kazancev.alex2020@gmail.com>
2025-09-07 20:30:37 +0300

    refactor: delete send(whom: WhomEnum, user?: User)

--- src/answers/3-refactoring-task.ts
+++ src/answers/3-refactoring-task.ts
@@ -1,4 +1,3 @@
-// Были конфликты имен, поэтому, для изоляции области видимости, я добавал {}
 {
   class Socket {
     send(user: User) {}
@@ -6,38 +5,16 @@
 
   class User {}
 
-  enum WhomEnum {
-    Self,
-    Other,
-    All,
-  }
-
   const socket = new Socket();
 
   class Server {
     readonly users: User[] = [];
 
-    send(whom: WhomEnum, user?: User): void {
-      switch (whom) {
-        case WhomEnum.Self:
-          this.sendToSelf(user);
-          break;
-        case WhomEnum.Other:
-          this.sendToOthers(user);
-          break;
-        case WhomEnum.All:
-          this.sendToAll();
-          break;
-      }
+    sendToSelf(user: User): void {
+      socket.send(user);
     }
 
-    private sendToSelf(user?: User): void {
-      if (user) {
-        socket.send(user);
-      }
-    }
-
-    private sendToOthers(excludedUser?: User): void {
+    sendToOthers(excludedUser: User): void {
       this.users.forEach((_user) => {
         if (_user !== excludedUser) {
           socket.send(_user);
@@ -45,7 +22,7 @@
       });
     }
 
-    private sendToAll(): void {
+    sendToAll(): void {
       this.users.forEach((user) => socket.send(user));
     }
   }
